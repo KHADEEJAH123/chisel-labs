794dec42728f74bfbaa2784db97c18d7e64bdaa1
KHADEEJAH ASHRAF <110920576+KHADEEJAH123@users.noreply.github.com>
2022-09-07 23:35:02 +0500

    Add files via upload

---
+++ src/main/scala/LABS/TASK1.scala
@@ -0,0 +1,11 @@
+package LABS
+import chisel3._
+class AND extends Module{
+     val io=IO(new Bundle{
+         val a= Input(UInt(4.W))
+         val b= Input(UInt(4.W))
+         val c= Output(UInt(4.W))
+         
+    })
+    io.c :=io.a.asUInt & io.b
+}
